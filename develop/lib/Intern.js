2745b610b1c9c9ac5c978e024e004877a1350f47
Jorge Morales <jmorales113@gmail.com>
2020-03-11 20:41:27 -0700

    Added properties and methods/passed test.

--- develop/lib/Intern.js
+++ develop/lib/Intern.js
@@ -1,1 +1,19 @@
+// Importing Employee clas
+const Employee = require("./Employee")
+// Extending Employee class to Intern class
+
+class Intern extends Employee {
+    constructor(name, id, email, title = "Intern", school = "UCLA") {
+        super(name, id, email, title)
+        this.school = school
+    }
+    getSchool() {
+        return this.school
+    }
+    getRole() {
+        return "Intern"
+    }
+}
+
+module.exports = Intern
 
