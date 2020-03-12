bbdfccdaa1969681657e12c5f0b17abd0f20ea7c
Jorge Morales <jmorales113@gmail.com>
2020-03-11 17:51:08 -0700

    Added properities and methods/passed test

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,1 +1,30 @@
+class Employee {
+    //Constructor Properties
+    constructor(name, id, email, title = "Employee"){
+        this.name = name
+        this.id = id
+        this.email = email
+        this.title = title
+    }
+    //Methods
 
+    getName() {
+        return this.name
+    }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getTitle() {
+        return this.title
+    }
+    getRole() {
+        return "Employee"
+    }
+}
+
+// Exporting class Employee
+
+module.exports = Employee
