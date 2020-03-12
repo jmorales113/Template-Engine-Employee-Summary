8fd3d676cb0a85bfe5505d5b6c2c53d259515120
Jorge Morales <jmorales113@gmail.com>
2020-03-11 19:21:16 -0700

    Fixed title

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -2,8 +2,8 @@
 const Employee = require("./Employee")
 // Extending Employee class to Engineer class
 class Engineer extends Employee {
-    constructor(name, id, email, title = "Employee", github = "GitHubUser") {
-        super(name, id, email, title = "Engineer")
+    constructor(name, id, email, title = "Engineer", github = "GitHubUser") {
+        super(name, id, email, title)
         this.github = github
     }
     getGithub() {
