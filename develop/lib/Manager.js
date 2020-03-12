c4f6723d0f4b6da0aaaae5280e4b403c36e50953
Jorge Morales <jmorales113@gmail.com>
2020-03-11 20:52:50 -0700

    Added properties and methods/Passed test

--- develop/lib/Manager.js
+++ develop/lib/Manager.js
@@ -1,1 +1,19 @@
+const Employee = require("./Employee")
 
+class Manager extends Employee {
+    constructor(name, id, email, title= "Manager", officeNumber = 100) {
+        super(name, id, email, title) 
+        this.officeNumber = officeNumber
+            {
+        }
+    }
+    getOfficeNumber() {
+        return this.officeNumber
+    }
+    getRole() {
+        return "Manager"
+    }
+
+}
+
+module.exports = Manager
