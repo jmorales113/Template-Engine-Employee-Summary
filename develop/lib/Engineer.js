d5749351c355a207c73a66d2deb15d5b1e10ab0c
Jorge Morales <jmorales113@gmail.com>
2020-03-11 18:58:37 -0700

    Extended Employee and passed test.

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,19 @@
+// Importing Employee class
+const Employee = require("./Employee")
+// Extending Employee class to Engineer class
+class Engineer extends Employee {
+    constructor(name, id, email, title = "Employee", github = "GitHubUser") {
+        super(name, id, email, title = "Employee")
+        this.github = github
+    }
+    getGithub() {
+        return this.github
+    }
+    getRole() {
+        return "Engineer"
+    }
+}
 
+// Exporting Engineer class
+
+module.exports = Engineer
