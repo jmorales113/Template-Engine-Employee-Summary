013ec082f0c1160fa6d2ab53c694318a7e0cfd9e
Jorge Morales <jmorales113@gmail.com>
2020-03-12 22:34:15 -0700

    Fixed whitespace error

--- develop/app.js
+++ develop/app.js
@@ -1,4 +1,5 @@
 const Manager = require("./lib/Manager");
+console.log(Manager)
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
@@ -9,36 +10,29 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 const team = []
-
 function manager() {
     inquirer.prompt([
-
     {
         type: "input",
         message: "Enter the manager's name: ",
         name: "name"
-    
     },
     {
         type: "input",
         message: "Enter the manager's ID: ",
         name: "id"
-
     },
     {
         type: "input",
         message: "Enter the manager's Email: ",
         name: "email"
-
     },
     {
         type: "input",
         message: "Enter the manager's office number: ",
         name: "officeNumber"
-
-    }
-    ​]).then(response => {
-        const mgr = new Manager(response.name, response.id, response.email, response.officeNumber)
+    }]).then(response=>{
+        var mgr = new Manager(response.name, response.id, response.email, response.officeNumber)
         team.push(mgr)
         addEmployees()
     })
@@ -69,10 +63,8 @@ function engineer() {
         type: "input",
         message: "Enter the engineer's GitHub username: ",
         name: "gitHub"
-
-    }
-    ​]).then(response => {
-      const eng = new Engineer(response.name, response.id, response.email, response.gitHub)
+    }]).then(response=>{
+        let eng = new Engineer(response.name, response.id, response.email, response.gitHub)
         team.push(eng)
         addEmployees()
     })
@@ -103,10 +95,8 @@ function intern() {
         type: "input",
         message: "Enter the intern's school: ",
         name: "school"
-
-    }
-    ​]).then(response => {
-        const int = new Intern(response.name, response.id, response.email, response.school)
+}]).then(response=>{
+        let int = new Intern(response.name, response.id, response.email, response.school)
         team.push(int)
         addEmployees()
     })
@@ -114,7 +104,7 @@ function intern() {
 
 function addEmployees() {
     inquirer.prompt([
-        
+
         {
             type: "list",
             message: "Add an employee?",
@@ -144,26 +134,3 @@ function addEmployees() {
     })
 }
 addEmployees()
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
